1a96569f46e8d7f1ec9c9c32cd641da4c2834d50
Sofia Bakke <sofiabakke@gmail.com>
2020-09-05 19:53:40 +0200

    Start

---
+++ src/main/kotlin/com/baje/cookbook/CookBookApplication.kt
@@ -0,0 +1,11 @@
+package com.baje.cookbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CookBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<CookBookApplication>(*args)
+}
---
+++ src/test/kotlin/com/baje/cookbook/CookBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baje.cookbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CookBookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
